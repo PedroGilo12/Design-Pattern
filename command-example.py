f1c33a09d7e4fbb7ab32d3a4beb38210054e4254
Pedro Giló <pedro.gilo@edge.ufal.br>
2023-04-17 09:41:37 -0300

    Adicionando operação para desligar a lampada

--- command-example.py
+++ command-example.py
@@ -18,6 +18,11 @@ class LampadaSmart:
         print("A lampada está ligada!")
         return self.estado
     
+    def desligar(self):
+        self.estado = False
+        print("A lampada está desligada!")
+        return self.estado
+    
     def incrementarIntensidade(self):
         self.intensidade = self.intensidade + 1
         print("O brilho atual é {}%!".format(self.intensidade))
@@ -26,7 +31,7 @@ class LampadaSmart:
 
 # Comando complexo
 
-class ComandoComplexo(Comando):
+class ligar(Comando):
 
     def __init__(self, receiver: LampadaSmart) -> None:
         self.receiver = receiver
@@ -34,24 +39,44 @@ class ComandoComplexo(Comando):
     def execute(self) -> None:
         self.receiver.ligar()
 
+class desligar(Comando):
+
+    def __init__(self, receiver: LampadaSmart) -> None:
+        self.receiver = receiver
+
+    def execute(self) -> None:
+        self.receiver.desligar()
+
 # Invoker class
 class InterruptorSmart:
     __button1_pressed = None
+    __button1_against = None
     
-    def set_button1(self, command: Comando):
+    def set_button1_pressed(self, command: Comando):
         self.__button1_pressed = command
     
+    def set_button2_against(self, command: Comando):
+        self.__button1_against = command
+
     def pressed(self):
 
         if isinstance(self.__button1_pressed, Comando):
             self.__button1_pressed.execute()
+
+    def against(self):
+
+        if isinstance(self.__button1_against, Comando):
+            self.__button1_against.execute()
+
 
 
 if __name__ == "__main__":
 
     Lampada = LampadaSmart()
     Interruptor = InterruptorSmart()
-    Interruptor.set_button1(ComandoComplexo(Lampada))
+    Interruptor.set_button1_pressed(ligar(Lampada))
+    Interruptor.set_button2_against(desligar(Lampada))
 
     Interruptor.pressed()
+    Interruptor.against()
 
